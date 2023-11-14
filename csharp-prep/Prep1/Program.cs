6605590309c4b6787e7f864f98fe004ae03a7ca4
David Gjennestad <davigjenn@gmail.com>
2023-11-14 14:50:50 -0700

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /*
+        The program prompts the users for their first and last name.
+        The program then displays the variables into the Console.
+        */
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
