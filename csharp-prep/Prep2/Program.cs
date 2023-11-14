58ef67b5e14affb08628af36180ad9aa6d2c360a
David Gjennestad <davigjenn@gmail.com>
2023-11-14 14:49:01 -0700

    Finished Prep 1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,112 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        /*
+        The program gets a grade percentage from the user then
+        procceds to convert it to int to then compare it to a set value
+        to a certien letter grade.
+        */
+
+        Console.Write("What is your grade percentage? ");
+        string userGradePercentage = Console.ReadLine();
+        int gradePercentage = int.Parse(userGradePercentage);
+        int lastDigit = gradePercentage % 10;
+
+        char letter;
+        char sign;
+
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+            
+            if (lastDigit < 3)
+            {
+                sign = '-';
+            }
+
+            else
+            {
+                sign = ' ';
+            }
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+
+            if (lastDigit >= 7)
+            {
+                sign = '+';
+            }
+
+            else if (lastDigit < 3)
+            {
+                sign = '-';
+            }
+
+            else
+            {
+                sign = ' ';
+            }
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+
+            if (lastDigit >= 7)
+            {
+                sign = '+';
+            }
+
+            else if (lastDigit < 3)
+            {
+                sign = '-';
+            }
+
+            else
+            {
+                sign = ' ';
+            }
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+
+            if (lastDigit >= 7)
+            {
+                sign = '+';
+            }
+
+            else if (lastDigit < 3)
+            {
+                sign = '-';
+            }
+
+            else
+            {
+                sign = ' ';
+            }
+        }
+
+        else
+        {
+            letter = 'F';
+            sign = ' ';
+        }
+
+        // Checks to see if the user passed with the gradePercentage
+        // to print out a specfic string to the console.
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"Congrats you passed with the letter {letter}{sign}!");
+        }
+
+        else
+        {
+            Console.WriteLine($"Try again! You got a {letter}{sign}.");
+        }
     }
 }
